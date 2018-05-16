c58ac5c25974b59c7b909b7414c6f25c36bc9163
Your Name <you@example.com>
2018-05-16 07:44:57 +0530

    s constraints

--- students/urls.py
+++ students/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url, include
 from views import *
 urlpatterns = [
     url(r'^$', index, name="student__index"),
+    url(r'^constraints/$', constraints, name="student__constraints"),
 ]
--- students/views.py
+++ students/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import ast
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from models import StudentConstraintsModel
 from constraints.models import Constraints
@@ -9,12 +10,27 @@ from constraints.models import Constraints
 
 def index(request):
     # check if user has not filled the survey form yes/no
-    student = StudentConstraintsModel.objects.filter(user=request.user)
+    constraints = StudentConstraintsModel.objects.filter(user=request.user)
     
-    if not student.exists():
-        return HttpResponse('no')
+    if not constraints.exists():
+        return HttpResponseRedirect('/students/constraints/')
     return render(request,{})
 
 def constraints(request):
-    constraints = Constraints.objects.all()
+
+    print request.POST
+    constraints_db = Constraints.objects.all()
+    constraints = []
+    for c in constraints_db:
+        t = {}
+        t['id'] = c.id
+        t['question'] = c.question
+        t['options'] = ast.literal_eval(c.options)
+        constraints.append(t)
+
     return render(request,'studentconstraints.html',{'constraints':constraints})
+
+# def fill(request):
+#     pass
+
+#     constraints = Constraints.objects.all()
