71109ea626d2737a9b2e05d3d6058f7187e5ded2
Your Name <you@example.com>
2018-05-18 00:52:01 +0530

    Studenet Constraints

--- students/views.py
+++ students/views.py
@@ -18,16 +18,36 @@ def index(request):
 
 def constraints(request):
 
-    print request.POST
+    if request.method == "POST":
+        data = request.POST
+        for c in request.POST.keys():
+            if c != 'csrfmiddlewaretoken':
+                StudentConstraintsModel.objects.update_or_create(user=request.user,constraint=c,
+                defaults={
+                    'user':request.user,
+                    'constraint':Constraints.objects.get(pk=c),
+                    'response' : request.POST[c]
+                })
+
     constraints_db = Constraints.objects.all()
+
+    student_selected_constraints = StudentConstraintsModel.objects.filter(user=request.user)
+    
     constraints = []
+    
     for c in constraints_db:
         t = {}
         t['id'] = c.id
         t['question'] = c.question
         t['options'] = ast.literal_eval(c.options)
+        selected_db = StudentConstraintsModel.objects.filter(user=request.user,constraint=c.id)
+        if selected_db.exists():
+            selected_db = selected_db.first()
+            t['selected'] = selected_db.response
+        else:
+            t['selected'] = ''
         constraints.append(t)
-
+        
     return render(request,'studentconstraints.html',{'constraints':constraints})
 
 # def fill(request):
