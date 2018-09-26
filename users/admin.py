d9c4142687cf199418900d92f784bee401c11c54
nitish <nitisxp@gmail.com>
2018-09-26 22:57:02 +0530

    Changes

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from models import UserModel
 from django.contrib import admin
-
+admin.site.register(UserModel)
 # Register your models here.
