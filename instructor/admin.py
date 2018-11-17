9b820da0183d9fb081bae40dd34fbe834695e800
Nitish Kumar Rai <nitish@tuple.techh>
2018-11-17 03:50:11 +0000

    Chnges

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from models import *
 from django.contrib import admin
+admin.site.register([CourseModel, CourseHomeWorkModel,StudentCourseEnrolledModel,HomeworkGroup,HomeworkGroupMember,
+                     HomeworkGroupGrade,GroupCombinationModel,AppealGraderModel,PeerEvaluationModel])
 
 # Register your models here.
